9e310a5cff3ebf279250306ebab5b1d28a94b951
ksa607 <kilytje1>
2017-11-15 12:18:49 +0100

    Bug fixe

--- app/src/main/java/be/hogent/jensbuysse/metartaff/fragments/DetailFragment.java
+++ app/src/main/java/be/hogent/jensbuysse/metartaff/fragments/DetailFragment.java
@@ -60,9 +60,9 @@ public class DetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_detail, container, false);
-        if(metar != null){
-            updateUI();
-        }
+       // if(metar != null){
+       //     updateUI();
+       // }
         return v;
     }
 
